a78f351d00b59fb8c6a0aa4d800a3d37c626c466
Marco Auŕelio <marcoaureliojunqueira8@gmail.com>
2025-09-26 13:24:32 -0300

    Adicionando a Tabela e migration gateways

--- start/routes.ts
+++ start/routes.ts
@@ -1,8 +1,11 @@
 import router from '@adonisjs/core/services/router'
 import UsersController from '#controllers/users_controller'
 
+// Users Routes
 router.get('/users', [UsersController, 'index'])
 router.post('/users', [UsersController, 'store'])
 router.get('/users/:id', [UsersController, 'show'])
 router.put('/users/:id', [UsersController, 'update'])
 router.delete('/users/:id', [UsersController, 'destroy'])
+
+// gateways Routes
