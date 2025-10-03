681fecc0d510c6374453aa5ccc400609e527a391
Marco Auŕelio <marcoaureliojunqueira8@gmail.com>
2025-10-03 13:35:52 -0300

    problemas resolvidos

--- database/migrations/1759237343669_create_produts_table.ts
+++ database/migrations/1759237343669_create_produts_table.ts
@@ -1,7 +1,7 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'produts'
+  protected tableName = 'products'
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
