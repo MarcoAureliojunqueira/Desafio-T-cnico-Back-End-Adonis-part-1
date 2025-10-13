c7e226e3f6efcd9f31fc7cf678bcbacb9cafa175
Marco Auŕelio <marcoaureliojunqueira8@gmail.com>
2025-10-13 14:07:51 -0300

    desenvolvendo a parte de login no adonis 6

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,1 +1,28 @@
-   
+   import type { HttpContext } from '@adonisjs/core/http'
+import User from '../models/User'
+import hash from '@adonisjs/core/services/hash'
+
+export default class AuthController {
+  async login({ request, auth, response }: HttpContext) {
+    const { email, password } = request.only(['email', 'password'])
+
+    const user = await User.findBy('email', email)
+
+    if (!user || !(await hash.verify(user.password, password))) {
+      return response.unauthorized({ message: 'Credenciais inválidas' })
+    }
+
+    const token = await auth.use('api').generate(user)
+
+    return response.ok({
+      message: 'Login realizado com sucesso',
+      token,
+      user: {
+        id: user.id,
+        fullName: user.fullName,
+        email: user.email,
+        role: user.role,
+      },
+    })
+  }
+}
