696eba24a5eb020f623b04578afe692367f6c60a
Marco Auŕelio <marcoaureliojunqueira8@gmail.com>
2025-10-27 00:01:30 -0300

    Atualizando e corrigindo authentificação

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,22 +1,24 @@
-   import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 import User from '../models/user.js'
 import hash from '@adonisjs/core/services/hash'
-
 
 export default class AuthController {
   async login({ request, auth, response }: HttpContext) {
     const { email, password } = request.only(['email', 'password'])
 
- console.log(request.all())
+    console.log(request.all())
+
     const user = await User.findBy('email', email)
-  
-    if (!user || !(await hash.verify(password, user.password))) {
+
+    if (!user || !(await hash.verify(user.password, password))) {
       return response.unauthorized({ message: 'Credenciais inválidas' })
     }
- console.log('erro ---->', auth);
 
+    console.log('Usuário autenticado:', user.email)
 
     const token = await auth.use('api').generate(user)
+
+    console.log('Token gerado:', token)
 
     return response.ok({
       message: 'Login realizado com sucesso',
@@ -25,8 +27,6 @@ export default class AuthController {
         id: user.id,
         fullName: user.fullName,
         email: user.email,
-       
-        
       },
     })
   }
